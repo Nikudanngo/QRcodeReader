b44e14cf380ac355c06295823c027353de61f8bc
Nikudanngo <satoru_rutosa@icloud.com>
2021-09-08 21:57:48 +0900

    Initial Commit

---
+++ QRcodeReader/QRcodeReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRcodeReaderApp.swift
+//  QRcodeReader
+//
+//  Created by 遠藤さとる on 2021/09/08.
+//
+
+import SwiftUI
+
+@main
+struct QRcodeReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
