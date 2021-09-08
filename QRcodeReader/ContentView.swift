0e032cb482040f492e7534cadfae8bfd8a01fe24
Nikudanngo <satoru_rutosa@icloud.com>
2021-09-09 01:31:49 +0900

    add TabBar

--- QRcodeReader/ContentView.swift
+++ QRcodeReader/ContentView.swift
@@ -7,10 +7,58 @@
 
 import SwiftUI
 
+struct ScanView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color(red: 30/255, green: 144/255, blue: 200/255)
+            }
+            .navigationTitle("Scan")
+        }
+    }
+}
+
+struct CreateView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color(red: 30/255, green: 144/255, blue: 100/255)
+            }
+            .navigationTitle("Create")
+        }
+    }
+}
+
+struct SettingsView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color(red: 80/255, green: 100/255, blue: 255/255)
+            }
+            .navigationTitle("Setting")
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            ScanView()
+                .tabItem {
+                    Image(systemName: "qrcode.viewfinder")
+                    Text("Scan")
+                }
+            CreateView()
+                .tabItem {
+                    Image(systemName: "qrcode")
+                    Text("Create")
+                }
+            SettingsView()
+                .tabItem {
+                    Image(systemName: "gearshape")
+                    Text("Setting")
+                }
+        }
     }
 }
 
