fd1653d46fb018325072bd6296436dd9f15b83bb
Nikudanngo <satoru_rutosa@icloud.com>
2021-09-11 19:23:09 +0900

    add SettingView.swift

--- QRcodeReader/ContentView.swift
+++ QRcodeReader/ContentView.swift
@@ -55,42 +55,64 @@ struct HistorysView: View {
         }
     }
 }
-//      Settingページ
-struct SettingsView: View {
-    var body: some View {
-        NavigationView {
-            ZStack {
-                Color("label")
-            }
-            .navigationTitle("Setting")
-        }
-    }
-}
+//　    　SettingページはSettingView.swiftに移動したよ
+//struct SettingsView: View {
+//    var body: some View {
+//        NavigationView {
+//            ZStack {
+//                Color("label")
+//            }
+//            .navigationTitle("Setting")
+//        }
+//    }
+//}
 // TabBarの処理
 //     アイコンを押すとそれぞれのページに変遷
 struct ContentView: View {
+    // ついでにスワイプに対応してタブを切り替えられるようにする
+    @State private var selectedTab = 0
+    // tabView(引数){}で引数に用意したtagに飛ぶことができるのでこれを利用する
+    
+// これでスワイプの感度を確かめる
+//    private func handleSwipe(translation: CGFloat) {
+//        print("handling swipe! horizontal translation was \(translation)")
+//    }
+    let minDragTranslationForSwipe: CGFloat = 100 // スワイプの感度を設定
+    let numTabs = 4     //タブの上限を設定
+    // スワイプを検知するとタブを表示する引数を上下させる
+    private func handleSwipe(translation: CGFloat) {
+        if translation > minDragTranslationForSwipe && selectedTab > 0 {
+            selectedTab -= 1
+        } else  if translation < -minDragTranslationForSwipe && selectedTab < numTabs-1 {
+            selectedTab += 1
+        }
+    }
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab) {
             ScanView()
                 .tabItem {
                     Image(systemName: "qrcode.viewfinder")
                     Text("Scan")
-                }
+                }.tag(0)
+                .highPriorityGesture(DragGesture().onEnded({ self.handleSwipe(translation: $0.translation.width)}))
             CreateView()
                 .tabItem {
                     Image(systemName: "qrcode")
                     Text("Create")
-                }
+                }.tag(1)
+                .highPriorityGesture(DragGesture().onEnded({ self.handleSwipe(translation: $0.translation.width)}))
             HistorysView()
                 .tabItem {
                     Image(systemName: "clock.arrow.circlepath")
                     Text("History")
-                }
+                }.tag(2)
+                .highPriorityGesture(DragGesture().onEnded({ self.handleSwipe(translation: $0.translation.width)}))
             SettingsView()
                 .tabItem {
                     Image(systemName: "gearshape")
                     Text("Setting")
-                }
+                }.tag(3)
+                .highPriorityGesture(DragGesture().onEnded({ self.handleSwipe(translation: $0.translation.width)}))
         }
     }
 }
---
+++ QRcodeReader/SettingView.swift
@@ -0,0 +1,90 @@
+//
+//  SettingView.swift
+//  QRcodeReader
+//
+//  Created by 遠藤さとる on 2021/09/11.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    
+    @State private var isOn = false
+    @State private var pickerSelection = 0
+    
+    let languages: [String] = [
+        "日本語"
+//      "English",みたいにすると選択肢増やせる
+    ]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("アップグレードする")) {
+                    HStack {
+                        Text("プレミアムへ")
+                        Spacer()
+                        Toggle(isOn: $isOn) {
+                            EmptyView()
+                        }
+                    }
+                    HStack {
+                        Picker(selection: $pickerSelection, label: Text("Language")) {
+                            ForEach(0..<self.languages.count) { index in
+                                Text(self.languages[index])
+                            }
+                        }
+                    }
+                    HStack {
+                        Picker(selection: $pickerSelection, label: Text("広告を見て全ての機能を有効にする")) {
+                            ForEach(0..<self.languages.count) { index in
+//                                Text(self.languages[index])
+                            }
+                        }
+                    }
+                }
+                Section(header: Text("カスタム設定")) {
+                    HStack {
+                        Text("広告を減らす")
+                        Spacer()
+                        Toggle(isOn: $isOn) {
+                            EmptyView()
+                        }
+                    }
+                    HStack {
+                        Text("履歴を保護する")
+                        Spacer()
+                        Toggle(isOn: $isOn) {
+                            EmptyView()
+                        }
+                    }
+                }
+                Section(header: Text("その他")) {
+                    HStack {
+                        Picker(selection: $pickerSelection, label: Text("権利を確認する")) {
+                            ForEach(0..<self.languages.count) { index in
+//                                Text(self.languages[index])
+                            }
+                        }
+                        
+                    }
+                    HStack {
+                        Picker(selection: $pickerSelection, label: Text("利用規約を確認する")) {
+                            ForEach(0..<self.languages.count) { index in
+//                                Text(self.languages[index])
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("Settings")
+        }
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+            .previewDevice("iPhone 12 Pro Max")
+    }
+}
